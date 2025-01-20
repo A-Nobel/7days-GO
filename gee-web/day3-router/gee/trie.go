8079673fce5be5bf015b880560ec7d5dad8d3745
LFshao <1445081825@qq.com>
2025-01-21 05:13:07 +0800

    feature: gee web 字典树 router

---
+++ gee-web/day3-router/gee/trie.go
@@ -0,0 +1,82 @@
+package gee
+
+import (
+	"fmt"
+	"strings"
+)
+
+type node struct {
+	pattern  string
+	part     string
+	children []*node
+	isWild   bool
+}
+
+func (n *node) String() string {
+	return fmt.Sprintf("node{pattern=%s, part=%s, isWild=%t}", n.pattern, n.part, n.isWild)
+}
+
+func (n *node) insert(pattern string, parts []string, height int) {
+	if len(parts) == height {
+		n.pattern = pattern
+		return
+	}
+	part := parts[height]
+	child := n.matchChild(part)
+	if child == nil {
+		child = &node{part: part, isWild: part[0] == ':' || part[0] == '*'}
+		n.children = append(n.children, child)
+	}
+	child.insert(pattern, parts, height+1)
+}
+
+// Find the target node
+func (n *node) search(parts []string, height int) *node {
+	if len(parts) == height || strings.HasPrefix(n.part, "*") {
+		// If it is not the leaf node, return nil
+		if n.pattern == "" {
+			return nil
+		}
+		return n
+	}
+	part := parts[height]
+	children := n.matchChildren(part)
+	for _, child := range children {
+		res := child.search(parts, height+1)
+		if res != nil {
+			return res
+		}
+	}
+	return nil
+}
+
+// Find all leaf node
+func (n *node) travel(list *[]*node) {
+	if n.pattern != "" {
+		*list = append(*list, n)
+	}
+	for _, child := range n.children {
+		child.travel(list)
+	}
+}
+
+// Match the First child node which has the target pattern
+func (n *node) matchChild(part string) *node {
+	for _, tmpNode := range n.children {
+		if tmpNode.part == part || tmpNode.isWild {
+			return tmpNode
+		}
+	}
+	return nil
+}
+
+// Match all the target children
+func (n *node) matchChildren(part string) []*node {
+	nodes := make([]*node, 0)
+	for _, child := range n.children {
+		if child.part == part || child.isWild {
+			nodes = append(nodes, child)
+		}
+	}
+	return nodes
+}
